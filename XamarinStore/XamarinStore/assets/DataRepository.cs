d48d29fb5098357b7ef16ed63aba1c934d63ec90
Ramón Guardia López <Mc_Ramy13@hotmail.com>
2017-11-29 18:01:57 +0100

    v 0.3
    Added data methods and create table

--- XamarinStore/XamarinStore/assets/DataRepository.cs
+++ XamarinStore/XamarinStore/assets/DataRepository.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using XamarinStore.model;
 
 namespace XamarinStore.assets
 {
@@ -16,10 +17,24 @@ namespace XamarinStore.assets
         {
             // TODO: Initialize a new SQLiteConnection
             conn = new SQLiteConnection(dbPath);
-            // TODO: Crear todas las tablas
-            //conn.CreateTable<Person>();
+            createTables(conn);
         }
 
+        //---------------- [ table creations ] -----------------
+        private void createTables(SQLiteConnection conn)
+        {
+            // TODO: Crear todas las tablas
+            conn.CreateTable<Case>();
+            conn.CreateTable<CPU>();
+            conn.CreateTable<GPU>();
+            conn.CreateTable<MotherBoard>();
+            conn.CreateTable<Pedido>();
+            conn.CreateTable<Ram>();
+            conn.CreateTable<User>();
+        }
+        //---------------- [ table creations ] -----------------
+
+        //---------------- [ get data methods ] -----------------
         public void AddNewPerson(string name)
         {
             int result = 0;
@@ -41,15 +56,15 @@ namespace XamarinStore.assets
 
         }
 
-        /*public List<Person> GetAllPeople()
+        public List<Case> GetAllCases()
         {
-            //Variable con lista de personas
-            List<Person> listPerson = new List<Person>();
+            //Variable con lista de cases
+            List<Case> casesList = new List<Case>();
 
             try
             {
-                // TODO: return a list of people saved to the Person table in the database
-                listPerson = conn.Table<Person>().ToList();
+                // TODO: return a list of cases saved to the Case table in the database
+                casesList = conn.Table<Case>().ToList();
             }
             catch (Exception ex)
             {
@@ -57,7 +72,123 @@ namespace XamarinStore.assets
             }
 
             //Se devuelve la lista de personas
-            return listPerson;
-        }*/
+            return casesList;
+        }
+
+        public List<CPU> GetAllCpu()
+        {
+            //Variable con lista de cases
+            List<CPU> cpuList = new List<CPU>();
+
+            try
+            {
+                // TODO: return a list of cases saved to the Case table in the database
+                cpuList = conn.Table<CPU>().ToList();
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = string.Format("Failed to retrieve data. {0}", ex.Message);
+            }
+
+            //Se devuelve la lista de personas
+            return cpuList;
+        }
+
+        public List<GPU> GetAllGpu()
+        {
+            //Variable con lista de cases
+            List<GPU> gpuList = new List<GPU>();
+
+            try
+            {
+                // TODO: return a list of cases saved to the Case table in the database
+                gpuList = conn.Table<GPU>().ToList();
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = string.Format("Failed to retrieve data. {0}", ex.Message);
+            }
+
+            //Se devuelve la lista de personas
+            return gpuList;
+        }
+
+        public List<MotherBoard> GetAllMotherBoards()
+        {
+            //Variable con lista de cases
+            List<MotherBoard> motherBoardList = new List<MotherBoard>();
+
+            try
+            {
+                // TODO: return a list of cases saved to the Case table in the database
+                motherBoardList = conn.Table<MotherBoard>().ToList();
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = string.Format("Failed to retrieve data. {0}", ex.Message);
+            }
+
+            //Se devuelve la lista de personas
+            return motherBoardList;
+        }
+
+        public List<Pedido> GetAllPedidos()
+        {
+            //Variable con lista de cases
+            List<Pedido> listPedidos = new List<Pedido>();
+
+            try
+            {
+                // TODO: return a list of cases saved to the Case table in the database
+                listPedidos = conn.Table<Pedido>().ToList();
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = string.Format("Failed to retrieve data. {0}", ex.Message);
+            }
+
+            //Se devuelve la lista de personas
+            return listPedidos;
+        }
+
+        public List<Ram> GetAllRam()
+        {
+            //Variable con lista de cases
+            List<Ram> listRam = new List<Ram>();
+
+            try
+            {
+                // TODO: return a list of cases saved to the Case table in the database
+                listRam = conn.Table<Ram>().ToList();
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = string.Format("Failed to retrieve data. {0}", ex.Message);
+            }
+
+            //Se devuelve la lista de personas
+            return listRam;
+        }
+
+        public List<User> GetAllUsers()
+        {
+            //Variable con lista de cases
+            List<User> listUsers = new List<User>();
+
+            try
+            {
+                // TODO: return a list of cases saved to the Case table in the database
+                listUsers = conn.Table<User>().ToList();
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = string.Format("Failed to retrieve data. {0}", ex.Message);
+            }
+
+            //Se devuelve la lista de personas
+            return listUsers;
+        }
+        //---------------- [ get data methods ] -----------------
+
     }
 }
