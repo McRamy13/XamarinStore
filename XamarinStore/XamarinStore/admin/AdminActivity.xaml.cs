b6661a1b37fab9a24fb4f5e2affb9b1608159f65
Ramón Guardia López <Mc_Ramy13@hotmail.com>
2017-12-08 14:31:43 +0100

    v.0.13
    Displayed data on

--- XamarinStore/XamarinStore/admin/AdminActivity.xaml.cs
+++ XamarinStore/XamarinStore/admin/AdminActivity.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,7 +15,7 @@ namespace XamarinStore.admin
     public partial class AdminActivity : ContentPage
     {
         private User user;
-        
+
         //cambiamos el constructor por defecto dejándolo como privado
         private AdminActivity()
         {
@@ -26,7 +27,41 @@ namespace XamarinStore.admin
         {
             this.user = user;
             InitializeComponent();
+            LoadDataAsync();
 
+        }
+
+        private async Task LoadDataAsync()
+        {
+            List<Pedido> listPedidos = new List<Pedido>();
+            ObservableCollection<Order> orderList = new ObservableCollection<Order>();
+
+            //obtenemos la lista de los pedidos
+            listPedidos = await App.DataRepo.GetAllPedidos();
+            //vamos creando por cada elemento de la lista de pedidos un objeto order
+            foreach (Pedido p in listPedidos)
+            {
+                User user = await App.DataRepo.GetUserById(p.IdUser);
+                PcBox pcBox = await App.DataRepo.GetPcBoxById(p.IdCase);
+                CPU cpu = await App.DataRepo.GetCPUById(p.IdCpu);
+                GPU gpu = await App.DataRepo.GetGPUById(p.IdCpu);
+                MotherBoard motherBoard = await App.DataRepo.GetMotherBoardId(p.IdMotherBoard);
+                Ram ram = await App.DataRepo.GetRamById(p.IdRam);
+                double price = p.Price;
+                //agregamos la información a el objeto Order
+                orderList.Add(new Order
+                {
+                    NomUser = user.Nick,
+                    NomCase = pcBox.Name,
+                    NomCpu = cpu.Name,
+                    NomGpu = gpu.Name,
+                    NomMotherBoard = motherBoard.Name,
+                    NomRam = ram.Name,
+                    FinalPrice = price
+                });
+            }
+            //Una vez agregamos todos los datos, pasamos los datos a la colección.
+            lstPedidos.ItemsSource = orderList;
         }
     }
 }
---
+++ XamarinStore/XamarinStore/model/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamarinStore.model
+{
+    public class Order
+    {
+        public string NomUser { get; set; }
+        public string NomCase { get; set; }
+        public string NomCpu { get; set; }
+        public string NomGpu { get; set; }
+        public string NomMotherBoard { get; set; }
+        public string NomRam { get; set; }
+        public double FinalPrice { get; set; }
+    }
+}
